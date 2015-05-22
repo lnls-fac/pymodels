0b82881fac188b4d428ccf878fe930b88a5fb5ea
ximenes <xresende@gmail.com>
2015-05-21 23:28:34 -0300

    Create SY subsystem
    - Synchronization subsystem

--- __init__.py
+++ __init__.py
@@ -1,7 +1,9 @@
 from . import SI_V07
 from . import BO_V901
+from . import SY_V00
 
-__all__ = ['SI_V07', 'BO_V901']
+__all__ = ['SI_V07', 'BO_V901', 'SY_V00']
 
 si = SI_V07
 bo = BO_V901
+sy = SY_V00
