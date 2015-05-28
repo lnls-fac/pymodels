8216f696e7e419cd2fb68c617972522a659d90d7
Afonso Haruo Carnielli Mukai <afonso.mukai@lnls.br>
2015-05-28 13:01:55 -0300

    Add setup.py

--- sirius/__init__.py
+++ sirius/__init__.py
@@ -1,7 +1,11 @@
+import os as _os
 from . import LI_V00
 from . import BO_V901
 from . import SI_V07
 from . import TI_V00
+
+with open(_os.path.join(__path__[0], 'VERSION'), 'r') as _f:
+    __version__ = _f.read().strip()
 
 __all__ = ['LI_V00', 'BO_V901', 'SI_V07', 'TI_V00']
 
