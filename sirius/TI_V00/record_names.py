6dd43f5fcd6af5582423af5a34a5fcf273026f1b
luana <luana.vilela@lnls.br>
2015-07-29 10:49:07 -0300

    Change names of timing pvs

--- sirius/TI_V00/record_names.py
+++ sirius/TI_V00/record_names.py
@@ -7,7 +7,7 @@ def get_record_names(family_name = None):
     values are the indices in the pyaccel model of the magnets
     that belong to the family. The magnet models ca be segmented,
     in which case the value is a python list of lists."""
-    
+
     if family_name == None:
         families = ['ti']
 
@@ -19,16 +19,19 @@ def get_record_names(family_name = None):
     if family_name.lower() == 'ti':
         _dict = {
                 'TI-CYCLE':{},               # when set starts entire injection cycle
-                'TI-BO-KICKIN-ON':{},        # whether to pulse BO injection kicker
+                'TI-BO-KICKIN-ENABLED':{},        # whether to pulse BO injection kicker
                 'TI-BO-KICKIN-DELAY':{},     # when to pulse BO injection kickes
-                'TI-BO-KICKEX-ON':{},        # whether to pulse BO extraction kicker
+                'TI-BO-KICKEX-ENABLED':{},        # whether to pulse BO extraction kicker
                 'TI-BO-KICKEX-DELAY':{},     # when to pulse BO extraction kicker
-                'TI-BO-KICKEX-INC':{},       # increment to SI injection kicker delay
-                'TI-SI-KICKIN-ON':{} ,       # whether to pulse SI injection kicker
+                'TI-SI-KICKIN-ENABLED':{} ,       # whether to pulse SI injection kicker
                 'TI-SI-KICKIN-DELAY':{},     # when to pulse SI injection kicker
-                #'TI-SI-KICKIN-INC':{},       # increment to SI injection kicker delay
+                'TI-DELAY':{},
+                'TI-DELAY-INC':{},
         }
         return _dict
 
     else:
         raise Exception('Family name %s not found'%family_name)
+
+
+ -
