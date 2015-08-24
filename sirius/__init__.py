8367711ea221c0250ab5d7f03bc299cf75fdad62
ximenes <xresende@gmail.com>
2015-08-24 12:32:47 -0300

    Add SI.V10
    - still broken but we I will merge with Luana's version

--- sirius/__init__.py
+++ sirius/__init__.py
@@ -1,7 +1,7 @@
 import os as _os
 from . import LI_V00
 from . import BO_V901
-from . import SI_V07
+from . import SI_V10
 from . import TS_V500
 from . import TB_V300
 from . import coordinate_system
@@ -9,10 +9,10 @@ from . import coordinate_system
 with open(_os.path.join(__path__[0], 'VERSION'), 'r') as _f:
     __version__ = _f.read().strip()
 
-__all__ = ['LI_V00', 'BO_V901', 'SI_V07', 'TS_V500', 'TB_V300']
+__all__ = ['LI_V00', 'BO_V901', 'SI_V10', 'TS_V500', 'TB_V300']
 
 li = LI_V00
 tb = TB_V300
 bo = BO_V901
 ts = TS_V500
-si = SI_V07
+si = SI_V10
