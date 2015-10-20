cd4fe7636d6d5254189de870f262cfa9c3c0461a
luana <luana.vilela@lnls.br>
2015-10-20 15:27:25 -0200

    Change booster default version (BO.V02)

--- sirius/__init__.py
+++ sirius/__init__.py
@@ -1,7 +1,6 @@
 import os as _os
 from . import LI_V00
 from . import TB_V01
-from . import BO_V901
 from . import BO_V02
 from . import TS_V01
 from . import SI_V12
@@ -11,10 +10,10 @@ from . import coordinate_system
 with open(_os.path.join(__path__[0], 'VERSION'), 'r') as _f:
     __version__ = _f.read().strip()
 
-__all__ = ['LI_V00', 'TB_V01', 'BO_V901', 'TS_V01', 'SI_V12', 'BO_V02']
+__all__ = ['LI_V00', 'TB_V01', 'BO_V02', 'TS_V01', 'SI_V12']
 
 li = LI_V00
 tb = TB_V01
-bo = BO_V901
+bo = BO_V02
 ts = TS_V01
 si = SI_V12
