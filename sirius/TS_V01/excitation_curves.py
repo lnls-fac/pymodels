9e9fd8c3d7c86d54d7755fa02602183888663bc9
luana <luana.vilela@lnls.br>
2015-10-21 12:42:30 -0200

    Fix excitation curves names

--- sirius/TS_V01/excitation_curves.py
+++ sirius/TS_V01/excitation_curves.py
@@ -7,7 +7,8 @@ _bend_re  = _re.compile('TSMA-B.*')
 _quad_re  = _re.compile('TSMA-Q.*')
 _hcorr_re = _re.compile('TSMA-CH.*')
 _vcorr_re = _re.compile('TSMA-CV.*')
-_sept_re  = _re.compile('TSPM-S.*')
+_septex_re  = _re.compile('TSPM-SEPTUME.*')
+_septin_re  = _re.compile('TSPM-SEPTUMT.*')
 
 def get_excitation_curve_mapping(accelerator):
     """Get mapping from magnet to excitation curve file names
@@ -26,8 +27,10 @@ def get_excitation_curve_mapping(accelerator):
             ec[name] = 'tsma-ch.txt'
         elif _vcorr_re.match(name) is not None:
             ec[name] = 'tsma-cv.txt'
-        elif _sept_re.match(name) is not None:
-            ec[name] = 'tspm-sep.txt'
+        elif _septex_re.match(name) is not None:
+            ec[name] = 'tspm-septex.txt'
+        elif _septin_re.match(name) is not None:
+            ec[name] = 'tspm-septin.txt'
         else:
             ec[name] = 'tsma-q.txt'
 
