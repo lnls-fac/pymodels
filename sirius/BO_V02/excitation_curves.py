ff43bece45d22fd7452a7bbe7ab8d317e3c1ae3c
Afonso Haruo Carnielli Mukai <afonso.mukai@lnls.br>
2015-10-21 09:59:06 -0200

    Update BO excitation curve names

--- sirius/BO_V02/excitation_curves.py
+++ sirius/BO_V02/excitation_curves.py
@@ -6,7 +6,8 @@ from . import record_names as _record_names
 _bend_re = _re.compile('BOMA-B.*')
 _qf_re = _re.compile('BOMA-QF.*')
 _qd_re = _re.compile('BOMA-QD.*')
-_s_re = _re.compile('BOMA-S.*')
+_sf_re = _re.compile('BOMA-SF.*')
+_sd_re = _re.compile('BOMA-SD.*')
 _ch_re = _re.compile('BOMA-CH.*')
 _cv_re = _re.compile('BOMA-CV.*')
 
@@ -23,9 +24,10 @@ def get_excitation_curve_mapping(accelerator):
         if _bend_re.match(name) is not None: ec[name] = 'boma-bend.txt'
         elif _qf_re.match(name) is not None: ec[name] = 'boma-qf.txt'
         elif _qd_re.match(name) is not None: ec[name] = 'boma-qd.txt'
-        elif _s_re.match(name) is not None: ec[name] = 'boma-s.txt'
+        elif _sf_re.match(name) is not None: ec[name] = 'boma-sf.txt'
+        elif _sd_re.match(name) is not None: ec[name] = 'boma-sd.txt'
         elif _ch_re.match(name) is not None: ec[name] = 'boma-ch.txt'
         elif _cv_re.match(name) is not None: ec[name] = 'boma-cv.txt'
-        else: ec[name] = 'boma-s.txt'
+        # else: ec[name] = 'boma-s.txt'
 
     return ec
