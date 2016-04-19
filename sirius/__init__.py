7ff244ba5d9178637f350e8442fc228df3b67fb3
Fernando <fernandohds564@gmail.com>
2016-04-19 19:21:37 -0300

    Creation of SI.V16.01

--- sirius/__init__.py
+++ sirius/__init__.py
@@ -3,18 +3,18 @@ from . import LI_V00
 from . import TB_V01
 from . import BO_V02A
 from . import TS_V01
-from . import SI_V15_01
+from . import SI_V16_01
 
 from . import coordinate_system
 
 with open(_os.path.join(__path__[0], 'VERSION'), 'r') as _f:
     __version__ = _f.read().strip()
 
-__all__ = ['LI_V00', 'TB_V01', 'BO_V02A', 'TS_V01', 'SI_V15_01']
+__all__ = ['LI_V00', 'TB_V01', 'BO_V02A', 'TS_V01', 'SI_V16_01']
 
 
 li = LI_V00
 tb = TB_V01
 bo = BO_V02A
 ts = TS_V01
-si = SI_V15_01
+si = SI_V16_01
