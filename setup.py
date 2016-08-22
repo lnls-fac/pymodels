9c53766fefb04bb3059084dd39f3ddf9278ffee4
Isabella Stevani <bellastevani@gmail.com>
2016-08-22 15:51:17 -0300

    Add BO_02A flat list

--- setup.py
+++ setup.py
@@ -19,6 +19,6 @@ setup(
         'Topic :: Scientific/Engineering'
     ],
     packages=find_packages(),
-    package_data={'sirius': ['VERSION']},
+    package_data={'sirius': ['VERSION','BO_V02A/at_flat_file_M0.txt']},
     zip_safe=False
 )
