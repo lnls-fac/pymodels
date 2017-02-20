679db46f4d2c2a32406a8b3bdcaa73804714e89b
ximenes <xresende@gmail.com>
2017-02-20 13:54:09 -0300

    Update in discs scripts

--- sirius/__init__.py
+++ sirius/__init__.py
@@ -5,11 +5,12 @@ from . import BO_V03_02
 from . import TS_V03_03
 from . import SI_V21_02
 from . import coordinate_system
+from . import discs
 
 with open(_os.path.join(__path__[0], 'VERSION'), 'r') as _f:
     __version__ = _f.read().strip()
 
-__all__ = ['LI_V01_01', 'TB_V01_03', 'BO_V03_02', 'TS_V03_03', 'SI_V2_02']
+__all__ = ['LI_V01_01', 'TB_V01_03', 'BO_V03_02', 'TS_V03_03', 'SI_V21_02']
 
 
 li = LI_V01_01
