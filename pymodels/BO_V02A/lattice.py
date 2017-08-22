fa9e8ba86e6b4ee8b148e8f8b647cdeb53e7b01e
ximenes <xresende@gmail.com>
2017-08-22 15:39:34 -0300

    Update VERSION and shebang in scripts

--- pymodels/BO_V02A/lattice.py
+++ pymodels/BO_V02A/lattice.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python-sirius
 
 import os as _os
 import math as _math
--- pymodels/BO_V03_02/lattice.py
+++ pymodels/BO_V03_02/lattice.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python-sirius
 
 import math as _math
 import numpy as _np
