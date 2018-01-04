0173fa2b81dfb672301c055f8c828c4cbd0f8875
ximenes <xresende@gmail.com>
2018-01-04 08:50:41 -0200

    Delete old model versions

--- pymodels/__init__.py
+++ pymodels/__init__.py
@@ -1,20 +1,26 @@
+"""PyModels package."""
+
 import os as _os
 from . import LI_V01_01
-from . import TB_V01_03
+# from . import TB_V01_03
+from . import TB_V02_01
 from . import BO_V03_02
 from . import TS_V03_03
-from . import SI_V21_02
+# from . import SI_V21_02
+from . import SI_V22_02
 from . import coordinate_system
 
 
 with open(_os.path.join(__path__[0], 'VERSION'), 'r') as _f:
     __version__ = _f.read().strip()
 
-__all__ = ['LI_V01_01', 'TB_V01_03', 'BO_V03_02', 'TS_V03_03', 'SI_V21_02']
+__all__ = ('LI_V01_01', 'TB_V02_01', 'BO_V03_02', 'TS_V03_03', 'SI_V22_02')
 
 
 li = LI_V01_01
-tb = TB_V01_03
+# tb = TB_V01_03
+tb = TB_V02_01
 bo = BO_V03_02
 ts = TS_V03_03
-si = SI_V21_02
+# si = SI_V21_02
+si = SI_V22_02
