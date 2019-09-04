7d9c846db14def0e6a5608ed4d92583624f58c98
Fernando <fernandohds564@gmail.com>
2019-09-04 16:24:17 -0300

    ENH: Add model for TB.V04.01 with skew in at the end of the line, together with CV.

---
+++ pymodels/TB_V04_01/__init__.py
@@ -0,0 +1,17 @@
+"""TB_V03_02 Model."""
+
+from .lattice import default_optics_mode
+from .lattice import energy
+
+from .accelerator import default_vchamber_on
+from .accelerator import default_radiation_on
+from .accelerator import accelerator_data
+from .accelerator import create_accelerator
+
+from .families import get_family_data
+from .families import family_mapping
+from .families import get_section_name_mapping
+
+from .control_system import get_control_system_data
+
+lattice_version = accelerator_data['lattice_version']
