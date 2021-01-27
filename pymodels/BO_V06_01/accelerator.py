f33cf1ecfdea838016b46c5e6e13e1d5fd5f4b5d
murilobalves <alvesb.murilo@gmail.com>
2021-01-27 16:56:51 -0300

    BO.MNT: Update lattice_version on accelerator_data

--- pymodels/BO_V06_01/accelerator.py
+++ pymodels/BO_V06_01/accelerator.py
@@ -24,7 +24,7 @@ def create_accelerator(optics_mode=_lattice.default_optics_mode, energy=_lattice
 
 
 accelerator_data = dict()
-accelerator_data['lattice_version'] = 'BO_V05_04'
+accelerator_data['lattice_version'] = 'BO_V06_01'
 accelerator_data['global_coupling'] = 0.0002  # expected corrected value
 accelerator_data['pressure_profile'] = _np.array([[0, 496.8], [1.5e-8]*2])  # [s [m], p [mbar]]o
 496.78745
