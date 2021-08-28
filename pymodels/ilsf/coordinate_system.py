c171e437b5cafac8e1e5160b7cdce5ec8e87e74b
ximenes <xresende@gmail.com>
2021-08-28 01:01:41 -0300

    Update coordinate systems

--- pymodels/ilsf/coordinate_system.py
+++ pymodels/ilsf/coordinate_system.py
@@ -5,18 +5,18 @@ def parameters(accelerator_prefix):
 
     Returns delta_rx[m] and delta_angle[rad]
     """
-    if accelerator_prefix == 'TB':
-        delta_rx = 0.0
-        delta_angle = 0.0
+    if accelerator_prefix == 'LI':
+        delta_rx = 0.0000
+        delta_angle = 0.0000
+    elif accelerator_prefix == 'TB':
+        delta_rx = 0.0000
+        delta_angle = 0.0000
     elif accelerator_prefix == 'BO':
-        delta_rx = -0.03
+        delta_rx = -0.0300
         delta_angle = 0.0143
     elif accelerator_prefix == 'TS':
-        # delta_rx = -0.022
-        # delta_angle = -0.005
-        # TODO: Update these values!!!
-        delta_rx = 0
-        delta_angle = 0
+        delta_rx = -0.0220
+        delta_angle = -0.0050
     elif accelerator_prefix == 'SI':
         delta_rx = -0.0165
         delta_angle = 0.00537
