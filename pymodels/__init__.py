8bd787d1d559f1a517014f9abba37fa8ede7c15d
ximenes <xresende@gmail.com>
2022-01-18 16:55:50 -0300

    Create SI_V25_02 model (with IDs modelled as kickmaps)

--- pymodels/__init__.py
+++ pymodels/__init__.py
@@ -8,16 +8,17 @@ from . import BO_V06_01
 from . import TS_V04_01
 from . import SI_V24_04
 from . import SI_V25_01
+from . import SI_V25_02
 from . import coordinate_system
 
 with open(_os.path.join(__path__[0], 'VERSION'), 'r') as _f:
     __version__ = _f.read().strip()
 
-__all__ = ('LI_V01_01', 'TB_V04_01', 'BO_V05_04', 'BO_V06_01', 'TS_V04_01', 'SI_V24_04', 'SI_V25_01')
+__all__ = ('LI_V01_01', 'TB_V04_01', 'BO_V05_04', 'BO_V06_01', 'TS_V04_01', 'SI_V24_04', 'SI_V25_01', 'SI_V25_02')
 
 
 li = LI_V01_01
 tb = TB_V04_01
 bo = BO_V06_01
 ts = TS_V04_01
-si = SI_V25_01
+si = SI_V25_02
