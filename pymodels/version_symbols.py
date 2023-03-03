9cfb355e1ec9246e91706dc246500f006e36acef
ximenes <xresende@gmail.com>
2023-03-03 17:47:28 -0300

    Fix comment

--- pymodels/version_symbols.py
+++ pymodels/version_symbols.py
@@ -4,4 +4,4 @@ from . import LI_V01_01 as li
 from . import TB_V04_01 as tb
 from . import BO_V06_01 as bo
 from . import SI_V25_04 as si
-from . import TS_V04_01 as ts  # NOTE: this imports needs to be after SI
+from . import TS_V04_01 as ts  # NOTE: this import needs to be after SI
