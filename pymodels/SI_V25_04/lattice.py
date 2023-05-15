61adcc65b8999484517f238d955b7bdc9bb2c838
gabriel <gabriel.ascencao@lnls.br>
2023-05-15 13:59:33 -0300

    add delta52 to straight section 10

--- pymodels/SI_V25_04/lattice.py
+++ pymodels/SI_V25_04/lattice.py
@@ -89,6 +89,7 @@ def create_lattice(
     L297 = drift('l297', 0.297)
     L300 = drift('l300', 0.300)
     L325 = drift('l325', 0.325)
+    L329 = drift('l230', 0.329)
     L336 = drift('l336', 0.336)
     L365 = drift('l365', 0.365)
     L400 = drift('l400', 0.400)
@@ -199,7 +200,7 @@ def create_lattice(
     ID07Hu, ID07Hd = kickmaps['ID07SP']  # CATERETE  'SI-07SP:ID-APU22'
     ID08Hu, ID08Hd = kickmaps['ID08SB']  # EMA       'SI-08SB:ID-IVU18'
     ID09Hu, ID09Hd = kickmaps['ID09SA']  # MANACA    'SI-09SA:ID-APU22'
-    ID10Hu, ID10Hd = kickmaps['ID10SB']  # SABIA     'SI-10SB:ID-EPU50'
+    ID10Hu, ID10Hd = kickmaps['ID10SB']  # SABIA     'SI-10SB:ID-DELTA52'
     ID11Hu, ID11Hd = kickmaps['ID11SP']  # IPE       'SI-11SP:ID-APU58'
     ID14Hu, ID14Hd = kickmaps['ID14SB']  # PAINEIRA  'SI-14SB:ID-WIG180'
     ID17Hu, ID17Hd = kickmaps['ID17SA']  # SAPUCAIA TEST PAPU50
@@ -360,9 +361,9 @@ def create_lattice(
         L500p, LID3, L500]  # high beta ID straight section (MANACA)
 
     IDB_10 = [
-        L297, L576p, IDQS, L203, IDBPM, L109, IDC, L218p,
+        L297, L576p, L200, L203, IDBPM, L665, L329, L218p,
         MIDB, ID10Hu, MIB, ID10Hd, MIDB,
-        L218p, IDC, L109, IDBPM, L203, IDQS, L576p, L297]  # low beta (SABIA)
+        L218p, L329, L665, IDBPM, L203, L200, L576p, L297]  # low beta (SABIA)
 
     IDP_11 = [
         L500, LIP, L500, L350p,
@@ -948,7 +949,7 @@ def create_id_kickmaps_dict(ids, energy):
         'ID07SP': ('APU22',  1.300),   # CATERETE
         'ID08SB': ('IVU18',  2.000),   # EMA
         'ID09SA': ('APU22',  1.300),   # MANACA
-        'ID10SB': ('EPU50',  2.770),   # SABIA
+        'ID10SB': ('DELTA52', 1.200),   # SABIA
         'ID11SP': ('APU58',  1.300),   # IPE
         'ID14SB': ('WIG180', 2.654),   # PAINEIRA
         'ID17SA': ('PAPU50', 1.200),   # SAPUCAIA
